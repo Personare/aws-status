10cbedbe67e2cc1487bce1faf4806fe1daba62b4
Cauê Alves <cauealveseng@gmail.com>
2016-08-20 20:27:04 -0300

    Refactoring for get only once service status

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
 'use strict';
 
-module.exports = (region, services) => {
+module.exports = (region, service) => {
     if (typeof region !== 'string') {
         throw new TypeError(`Expected a string but got ${typeof region}`);
     }
 
-    if (!Array.isArray(services)) {
-        throw new TypeError(`Expected an array but got ${typeof services}`);
+    if (typeof service !== 'string') {
+        throw new TypeError(`Expected a string but got ${typeof service}`);
     }
 
     return new Promise((resolve, reject) => {
         const getStatus = require('./getStatus');
 
-        let result = [];
-
-        const totalServices = services.length;
-        for (let i = 0; i < totalServices; i++) {
-            const service = services[i];
-            getStatus(region, service).then(status => {
-                result.push({
-                    service,
-                    status
-                });
-
-                if (i === (totalServices - 1)) {
-                    resolve(result);
-                }
+        getStatus(region, service).then(status => {
+            resolve({
+                service,
+                status
             });
-        }
+        }).catch(error => reject(error));
     });
 };
--- test.js
+++ test.js
@@ -3,30 +3,25 @@ import awsStatus from './';
 
 test('should throw exception with the invalid arguments', t => {
     t.throws(() => {
-        awsStatus(undefined, ['EC2', 'RDS']);
+        awsStatus(undefined, 'EC2');
     }, /Expected a string but got undefined/);
 
     t.throws(() => {
-        awsStatus(123, ['EC2', 'RDS']);
+        awsStatus(123, 'RDS');
     }, /Expected a string but got number/);
 
     t.throws(() => {
         awsStatus('us-east-1', false);
-    }, /Expected an array but got boolean/);
+    }, /Expected a string but got boolean/);
 
     t.throws(() => {
         awsStatus('us-east-1', {});
-    }, /Expected an array but got object/);
-
-    t.throws(() => {
-        awsStatus('us-east-1', 'EC2');
-    }, /Expected an array but got string/);
+    }, /Expected a string but got object/);
 });
 
-test('should return an array with the correct services', async t => {
-    const service = 'EC2';
-
-    await awsStatus('us-east-1', [service]).then(result => {
-        t.is(result[0].service, service);
+test('should return an object with the correct keys', async t => {
+    await awsStatus('us-east-1', 'EC2').then(result => {
+        t.true('service' in result);
+        t.true('status' in result);
     });
 });
